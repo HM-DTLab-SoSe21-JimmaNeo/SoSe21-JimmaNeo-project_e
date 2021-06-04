d8f54e7007a94f84a7251e431e8e4242b8f0c865
ivan1 <othmann@hm.edu>
2021-06-04 16:01:26 +0200

    Version2

---
+++ src/SEIIApp/Client/NewFolder3/PDFGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Client.NewFolder3
+{
+    public class PDFGenerator
+    {
+    }
+}
