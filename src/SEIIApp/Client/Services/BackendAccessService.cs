d0cff2167006ee4e5b3f519482126a8de6e9bde7
ivan1 <othmann@hm.edu>
2021-06-07 19:09:25 +0200

    Changes

--- src/SEIIApp/Client/Services/BackendAccessService.cs
+++ src/SEIIApp/Client/Services/BackendAccessService.cs
@@ -90,5 +90,53 @@ namespace SEIIApp.Client.Services
 
             return null;
         }
+
+        private string GetQuizUrl()
+        {
+            return "api/quizdefinitions";
+        }
+
+        private string GetQuizUrlWithId(int id)
+        {
+            return $"{GetQuizUrl()}/{id}";
+        }
+
+        /// <summary>
+        /// Returns a certain quiz by id
+        /// </summary>
+        public async Task<QuizDto> GetQuizById(int id)
+        {
+            return await HttpClient.GetFromJsonAsync<QuizDto>(GetQuizUrlWithId(id));
+        }
+
+        /// <summary>
+        /// Returns all quizzes stored on the backend
+        /// </summary>
+        public async Task<QuizDto[]> GetQuizOverview()
+        {
+            return await HttpClient.GetFromJsonAsync<QuizDto[]>(GetQuizUrl());
+        }
+
+        /// <summary>
+        /// Adds or updates a quiz on the backend. Returns the quiz if successful else null
+        /// </summary>
+        public async Task<QuizDto> AddOrUpdateQuiz(QuizDto dto)
+        {
+            var response = await HttpClient.PutAsJsonAsync(GetQuizUrl(), dto);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await response.DeserializeResponseContent<QuizDto>();
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// Deletes a quiz and returns true if successful
+        /// </summary>
+        public async Task<bool> DeleteQuiz(int quizId)
+        {
+            var response = await HttpClient.DeleteAsync(GetQuizUrlWithId(quizId));
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
     }
 }
